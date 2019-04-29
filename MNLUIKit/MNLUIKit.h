5b26ab604953defc546d8d5cee749e85dd5c91d6
Mayu Laierlence <minacle@live.com>
2019-04-29 16:53:39 +0900

    Add buttons to MNLUIKit.h

--- MNLUIKit/MNLUIKit.h
+++ MNLUIKit/MNLUIKit.h
@@ -2,6 +2,8 @@
 
 #import <MNLUIKit/MNLUIView.h>
 #import <MNLUIKit/MNLUIGradientView.h>
+#import <MNLUIKit/MNLUIButton.h>
+#import <MNLUIKit/MNLUIGradientButton.h>
 
 
 /// A version number for MNLUIKit.
